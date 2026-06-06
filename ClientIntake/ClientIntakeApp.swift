3b908c664208750ce0a2a73aa681b9a16553fb40
Katherine Petalio <katherine.petalio@gmail.com>
2026-06-06 21:37:34 +0800

    Initial Commit

---
+++ ClientIntake/ClientIntakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClientIntakeApp.swift
+//  ClientIntake
+//
+//  Created by Katherine Petalio on 6/6/26.
+//
+
+import SwiftUI
+
+@main
+struct ClientIntakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClientIntakeTests/ClientIntakeTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClientIntakeTests.swift
+//  ClientIntakeTests
+//
+//  Created by Katherine Petalio on 6/6/26.
+//
+
+import Testing
+
+struct ClientIntakeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
